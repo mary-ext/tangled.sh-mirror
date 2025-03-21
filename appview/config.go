9eb6245bccc7b21dd7a34ce4f2f315f8b1c711bd
Anirudh Oppiliappan <x@icyphox.sh>
2025-03-21 07:59:20 +0200

    appview/config: make jetstream endpoint configurable

--- appview/config.go
+++ appview/config.go
@@ -7,10 +7,11 @@ import (
 )
 
 type Config struct {
-	CookieSecret string `env:"TANGLED_COOKIE_SECRET, default=00000000000000000000000000000000"`
-	DbPath       string `env:"TANGLED_DB_PATH, default=appview.db"`
-	ListenAddr   string `env:"TANGLED_LISTEN_ADDR, default=0.0.0.0:3000"`
-	Dev          bool   `env:"TANGLED_DEV, default=false"`
+	CookieSecret      string `env:"TANGLED_COOKIE_SECRET, default=00000000000000000000000000000000"`
+	DbPath            string `env:"TANGLED_DB_PATH, default=appview.db"`
+	ListenAddr        string `env:"TANGLED_LISTEN_ADDR, default=0.0.0.0:3000"`
+	Dev               bool   `env:"TANGLED_DEV, default=false"`
+	JetstreamEndpoint string `env:"TANGLED_JETSTREAM_ENDPOINT, default=wss://jetstream1.us-east.bsky.network/subscribe"`
 }
 
 func LoadConfig(ctx context.Context) (*Config, error) {
