3f9986a420d2ceaa66ea35261ac2156cc0b69e85
Tangled <noreply@tangled.sh>
2025-03-23 17:54:27 +0000

    pass internal-api endpoint from keyfetch to repoguard
    no point in customizing the endpoint in keyfetch, if it is not propagated through to repoguard

--- cmd/keyfetch/format.go
+++ cmd/keyfetch/format.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 )
 
-func formatKeyData(repoguardPath, gitDir, logPath string, data []map[string]interface{}) string {
+func formatKeyData(repoguardPath, gitDir, logPath, endpoint string, data []map[string]interface{}) string {
 	var result string
 	for _, entry := range data {
 		result += fmt.Sprintf(
-			`command="%s -base-dir %s -user %s -log-path %s",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty %s`+"\n",
-			repoguardPath, gitDir, entry["did"], logPath, entry["key"])
+			`command="%s -base-dir %s -user %s -log-path %s -internal-api %s",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty %s`+"\n",
+			repoguardPath, gitDir, entry["did"], logPath, endpoint, entry["key"])
 	}
 	return result
 }
