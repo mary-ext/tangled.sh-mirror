c17b766cc0fe332c2765a638d760f3e5f481626b
oppiliappan <me@oppi.li>
2025-06-04 20:49:05 +0100

    nix: break out appview module into separate file
    Signed-off-by: oppiliappan <me@oppi.li>

--- flake.nix
+++ flake.nix
@@ -200,52 +200,6 @@
       };
     });
 
-    nixosModules.appview = {
-      config,
-      pkgs,
-      lib,
-      ...
-    }:
-      with lib; {
-        options = {
-          services.tangled-appview = {
-            enable = mkOption {
-              type = types.bool;
-              default = false;
-              description = "Enable tangled appview";
-            };
-            port = mkOption {
-              type = types.int;
-              default = 3000;
-              description = "Port to run the appview on";
-            };
-            cookie_secret = mkOption {
-              type = types.str;
-              default = "00000000000000000000000000000000";
-              description = "Cookie secret";
-            };
-          };
-        };
-
-        config = mkIf config.services.tangled-appview.enable {
-          systemd.services.tangled-appview = {
-            description = "tangled appview service";
-            wantedBy = ["multi-user.target"];
-
-            serviceConfig = {
-              ListenStream = "0.0.0.0:${toString config.services.tangled-appview.port}";
-              ExecStart = "${self.packages.${pkgs.system}.appview}/bin/appview";
-              Restart = "always";
-            };
-
-            environment = {
-              TANGLED_DB_PATH = "appview.db";
-              TANGLED_COOKIE_SECRET = config.services.tangled-appview.cookie_secret;
-            };
-          };
-        };
-      };
-
     nixosModules.knot = {
       config,
       pkgs,
@@ -448,5 +402,6 @@
         })
       ];
     };
+    nixosModules.appview = import ./nix/modules/appview.nix {inherit self;};
   };
 }
