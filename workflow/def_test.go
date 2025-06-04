46b2702eb4551814d6e8a7faca0d203e655b6f62
oppiliappan <me@oppi.li>
2025-06-04 12:45:18 +0100

    workflow: outline basic workflow format
    in order to work around the limitations of not having github actions'
    marketplace, this approach opts to use nixpkgs as a source for packages.
    alternate registries can be specified too, these are expected to be nix
    flakes that expose packages.
    
    this takes a page out of replit's approach to supplying packages to
    their devshells, however, instead of using nix syntax, we use only a
    flake + package combo, and the compiler will simply convert this into a
    step like so:
    
        nix profile install flake#package
    
    Signed-off-by: oppiliappan <me@oppi.li>

---
+++ workflow/def_test.go
@@ -0,0 +1,116 @@
+package workflow
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnmarshalWorkflow(t *testing.T) {
+	yamlData := `
+when:
+  - event: ["push", "pull_request"]
+    branch: ["main", "develop"]
+
+dependencies:
+  nixpkgs:
+    - go
+    - git
+    - curl
+
+steps:
+  - name: "Test"
+    command: |
+       go test ./...`
+
+	wf, err := FromFile("test.yml", []byte(yamlData))
+	assert.NoError(t, err, "YAML should unmarshal without error")
+
+	assert.Len(t, wf.When, 1, "Should have one constraint")
+	assert.ElementsMatch(t, []string{"main", "develop"}, wf.When[0].Branch)
+	assert.ElementsMatch(t, []string{"push", "pull_request"}, wf.When[0].Event)
+
+	assert.Len(t, wf.Steps, 1)
+	assert.Equal(t, "Test", wf.Steps[0].Name)
+	assert.Equal(t, "go test ./...", wf.Steps[0].Command)
+
+	pkgs, ok := wf.Dependencies["nixpkgs"]
+	assert.True(t, ok, "`nixpkgs` should be present in dependencies")
+	assert.ElementsMatch(t, []string{"go", "git", "curl"}, pkgs)
+
+	assert.False(t, wf.CloneOpts.Skip, "Skip should default to false")
+}
+
+func TestUnmarshalCustomRegistry(t *testing.T) {
+	yamlData := `
+when:
+  - event: push
+    branch: main
+
+dependencies:
+  git+https://tangled.sh/@oppi.li/tbsp:
+    - tbsp
+  git+https://git.peppe.rs/languages/statix:
+    - statix
+
+steps:
+  - name: "Check"
+    command: |
+       statix check`
+
+	wf, err := FromFile("test.yml", []byte(yamlData))
+	assert.NoError(t, err, "YAML should unmarshal without error")
+
+	assert.ElementsMatch(t, []string{"push"}, wf.When[0].Event)
+	assert.ElementsMatch(t, []string{"main"}, wf.When[0].Branch)
+
+	assert.ElementsMatch(t, []string{"tbsp"}, wf.Dependencies["git+https://tangled.sh/@oppi.li/tbsp"])
+	assert.ElementsMatch(t, []string{"statix"}, wf.Dependencies["git+https://git.peppe.rs/languages/statix"])
+}
+
+func TestUnmarshalCloneFalse(t *testing.T) {
+	yamlData := `
+when:
+  - event: pull_request_close
+
+clone:
+  skip: true
+
+dependencies:
+  nixpkgs:
+    - python3
+
+steps:
+  - name: Notify
+    command: |
+      python3 ./notify.py
+`
+
+	wf, err := FromFile("test.yml", []byte(yamlData))
+	assert.NoError(t, err)
+
+	assert.ElementsMatch(t, []string{"pull_request_close"}, wf.When[0].Event)
+
+	assert.True(t, wf.CloneOpts.Skip, "Skip should be false")
+}
+
+func TestUnmarshalEnv(t *testing.T) {
+	yamlData := `
+when:
+  - event: ["pull_request_close"]
+
+clone:
+  skip: false
+
+environment:
+  HOME: /home/foo bar/baz
+  CGO_ENABLED: 1
+`
+
+	wf, err := FromFile("test.yml", []byte(yamlData))
+	assert.NoError(t, err)
+
+	assert.Len(t, wf.Environment, 2)
+	assert.Equal(t, "/home/foo bar/baz", wf.Environment["HOME"])
+	assert.Equal(t, "1", wf.Environment["CGO_ENABLED"])
+}
