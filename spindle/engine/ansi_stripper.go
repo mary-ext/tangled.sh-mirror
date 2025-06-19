93cfd4e431f5d01176760db1f1baa4cb61e32fe4
Anirudh Oppiliappan <anirudh@tangled.sh>
2025-06-19 18:11:32 +0300

    spindle/{config,engine}: configure a timeout for steps
    WaitStep and TailStep now run in goroutines, and are tracked with a
    stepCtx which has a timeout attached. Once stepCtx expires, the step is
    killed with DestroyStep.
    
    The default timeout is set to 5m, and is configureable using
    SPINDLE_PIPELINES_STEP_TIMEOUT.
    
    Signed-off-by: Anirudh Oppiliappan <anirudh@tangled.sh>

--- spindle/engine/ansi_stripper.go
+++ spindle/engine/ansi_stripper.go
@@ -3,13 +3,13 @@ package engine
 import (
 	"io"
 
-	"github.com/go-enry/go-enry/v2/regex"
+	"regexp"
 )
 
 // regex to match ANSI escape codes (e.g., color codes, cursor moves)
 const ansi = "[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))"
 
-var re = regex.MustCompile(ansi)
+var re = regexp.MustCompile(ansi)
 
 type ansiStrippingWriter struct {
 	underlying io.Writer
