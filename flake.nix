87b0ac79eac4ef3e55ef86da3474fd806f76fefc
oppiliappan <me@oppi.li>
2025-07-16 17:58:42 +0100

    appview/pages: unify workflow styles with the rest of the website
    not many portions of the site use darkend backgrounds. this is very much
    WIP.
    
    Signed-off-by: oppiliappan <me@oppi.li>

--- flake.nix
+++ flake.nix
@@ -151,7 +151,7 @@
           ${pkgs.air}/bin/air -c /dev/null \
           -build.cmd "${pkgs.go}/bin/go build -o ./out/${name}.out ./cmd/${name}/main.go" \
           -build.bin "./out/${name}.out" \
-          -build.args_bin "${arg}"
+          -build.args_bin "${arg}" \
           -build.stop_on_error "true" \
           -build.include_ext "go"
         '';
