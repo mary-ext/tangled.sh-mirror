f664a31da36f49e56c53cc1f86d0fbc08d6fe52e
oppiliappan <me@oppi.li>
2025-07-29 13:17:05 +0100

    spindle: introduce secret manager interface
    Signed-off-by: oppiliappan <me@oppi.li>

---
+++ spindle/secrets/manager.go
@@ -0,0 +1,61 @@
+package secrets
+
+import (
+	"errors"
+	"regexp"
+	"time"
+
+	"github.com/bluesky-social/indigo/atproto/syntax"
+)
+
+type Secret[T any] struct {
+	Key       string
+	Value     T
+	Repo      syntax.ATURI
+	CreatedAt time.Time
+	CreatedBy syntax.DID
+}
+
+// the secret is not present
+type LockedSecret = Secret[struct{}]
+
+// the secret is present in plaintext, never expose this publicly,
+// only use in the workflow engine
+type UnlockedSecret = Secret[string]
+
+type Manager interface {
+	AddSecret(secret UnlockedSecret) error
+	RemoveSecret(secret Secret[any]) error
+	GetSecretsLocked(repo syntax.ATURI) ([]LockedSecret, error)
+	GetSecretsUnlocked(repo syntax.ATURI) ([]UnlockedSecret, error)
+}
+
+var ErrKeyAlreadyPresent = errors.New("key already present")
+var ErrInvalidKeyIdent = errors.New("key is not a valid identifier")
+var ErrKeyNotFound = errors.New("key not found")
+
+// ensure that we are satisfying the interface
+var (
+	_ = []Manager{
+		&SqliteManager{},
+	}
+)
+
+var (
+	// bash identifier syntax
+	keyIdent = regexp.MustCompile(`^[a-zA-Z_][a-zA-Z0-9_]*$`)
+)
+
+func isValidKey(key string) bool {
+	if key == "" {
+		return false
+	}
+	return keyIdent.MatchString(key)
+}
+
+func ValidateKey(key string) error {
+	if !isValidKey(key) {
+		return ErrInvalidKeyIdent
+	}
+	return nil
+}
