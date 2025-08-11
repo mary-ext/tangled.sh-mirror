5ca99c745096bc806559543a49834e4710ba3a59
Winter <winter@winter.cafe>
2025-08-11 18:11:14 -0400

    nix/pkgs/genjwks: use granular source
    I'm so sick of this thing rebuilding every time I reload my devshell.
    
    Signed-off-by: Winter <winter@winter.cafe>

--- nix/pkgs/genjwks.nix
+++ nix/pkgs/genjwks.nix
@@ -1,13 +1,15 @@
 {
-  src,
   buildGoApplication,
   modules,
 }:
 buildGoApplication {
   pname = "genjwks";
   version = "0.1.0";
-  inherit src modules;
-  subPackages = ["cmd/genjwks"];
+  src = ../../cmd/genjwks;
+  postPatch = ''
+    ln -s ${../../go.mod} ./go.mod
+  '';
+  inherit modules;
   doCheck = false;
   CGO_ENABLED = 0;
 }
