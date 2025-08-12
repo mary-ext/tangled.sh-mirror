c61ac86d1d0d3ba41dde2ae092a4dc64c0aaf378
Anirudh Oppiliappan <anirudh@tangled.sh>
2025-08-12 16:37:42 +0300

    knotserver/xrpc: use new top-level xrpc packages
    Signed-off-by: Anirudh Oppiliappan <anirudh@tangled.sh>

--- knotserver/xrpc/router.go
+++ knotserver/xrpc/router.go
@@ -1,12 +1,9 @@
 package xrpc
 
 import (
-	"context"
 	"encoding/json"
-	"fmt"
 	"log/slog"
 	"net/http"
-	"strings"
 
 	"tangled.sh/tangled.sh/core/api/tangled"
 	"tangled.sh/tangled.sh/core/idresolver"
@@ -15,134 +12,35 @@ import (
 	"tangled.sh/tangled.sh/core/knotserver/db"
 	"tangled.sh/tangled.sh/core/notifier"
 	"tangled.sh/tangled.sh/core/rbac"
+	xrpcerr "tangled.sh/tangled.sh/core/xrpc/errors"
+	"tangled.sh/tangled.sh/core/xrpc/serviceauth"
 
-	"github.com/bluesky-social/indigo/atproto/auth"
 	"github.com/go-chi/chi/v5"
 )
 
 type Xrpc struct {
-	Config   *config.Config
-	Db       *db.DB
-	Ingester *jetstream.JetstreamClient
-	Enforcer *rbac.Enforcer
-	Logger   *slog.Logger
-	Notifier *notifier.Notifier
-	Resolver *idresolver.Resolver
+	Config      *config.Config
+	Db          *db.DB
+	Ingester    *jetstream.JetstreamClient
+	Enforcer    *rbac.Enforcer
+	Logger      *slog.Logger
+	Notifier    *notifier.Notifier
+	Resolver    *idresolver.Resolver
+	ServiceAuth *serviceauth.ServiceAuth
 }
 
 func (x *Xrpc) Router() http.Handler {
 	r := chi.NewRouter()
 
-	r.With(x.VerifyServiceAuth).Post("/"+tangled.RepoSetDefaultBranchNSID, x.SetDefaultBranch)
+	r.With(x.ServiceAuth.VerifyServiceAuth).Post("/"+tangled.RepoSetDefaultBranchNSID, x.SetDefaultBranch)
 
 	return r
-}
-
-func (x *Xrpc) VerifyServiceAuth(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		l := x.Logger.With("url", r.URL)
-
-		token := r.Header.Get("Authorization")
-		token = strings.TrimPrefix(token, "Bearer ")
-
-		s := auth.ServiceAuthValidator{
-			Audience: x.Config.Server.Did().String(),
-			Dir:      x.Resolver.Directory(),
-		}
-
-		did, err := s.Validate(r.Context(), token, nil)
-		if err != nil {
-			l.Error("signature verification failed", "err", err)
-			writeError(w, AuthError(err), http.StatusForbidden)
-			return
-		}
-
-		r = r.WithContext(
-			context.WithValue(r.Context(), ActorDid, did),
-		)
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-type XrpcError struct {
-	Tag     string `json:"error"`
-	Message string `json:"message"`
-}
-
-func NewXrpcError(opts ...ErrOpt) XrpcError {
-	x := XrpcError{}
-	for _, o := range opts {
-		o(&x)
-	}
-
-	return x
-}
-
-type ErrOpt = func(xerr *XrpcError)
-
-func WithTag(tag string) ErrOpt {
-	return func(xerr *XrpcError) {
-		xerr.Tag = tag
-	}
-}
-
-func WithMessage[S ~string](s S) ErrOpt {
-	return func(xerr *XrpcError) {
-		xerr.Message = string(s)
-	}
-}
-
-func WithError(e error) ErrOpt {
-	return func(xerr *XrpcError) {
-		xerr.Message = e.Error()
-	}
-}
-
-var MissingActorDidError = NewXrpcError(
-	WithTag("MissingActorDid"),
-	WithMessage("actor DID not supplied"),
-)
-
-var AuthError = func(err error) XrpcError {
-	return NewXrpcError(
-		WithTag("Auth"),
-		WithError(fmt.Errorf("signature verification failed: %w", err)),
-	)
-}
-
-var InvalidRepoError = func(r string) XrpcError {
-	return NewXrpcError(
-		WithTag("InvalidRepo"),
-		WithError(fmt.Errorf("supplied at-uri is not a repo: %s", r)),
-	)
-}
-
-var AccessControlError = func(d string) XrpcError {
-	return NewXrpcError(
-		WithTag("AccessControl"),
-		WithError(fmt.Errorf("DID does not have sufficent access permissions for this operation: %s", d)),
-	)
-}
-
-var GitError = func(e error) XrpcError {
-	return NewXrpcError(
-		WithTag("Git"),
-		WithError(fmt.Errorf("git error: %w", e)),
-	)
-}
-
-func GenericError(err error) XrpcError {
-	return NewXrpcError(
-		WithTag("Generic"),
-		WithError(err),
-	)
 }
 
 // this is slightly different from http_util::write_error to follow the spec:
 //
 // the json object returned must include an "error" and a "message"
-func writeError(w http.ResponseWriter, e XrpcError, status int) {
+func writeError(w http.ResponseWriter, e xrpcerr.XrpcError, status int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(e)
