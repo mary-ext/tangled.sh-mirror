8de7701ee566d1cd09a28240d8af467b33bc29dd
oppiliappan <me@oppi.li>
2025-08-15 14:49:37 +0100

    input.css: fix fonts for good
    define variants for all families, weights and styles.
    
    Signed-off-by: oppiliappan <me@oppi.li>

--- nix/pkgs/appview-static-files.nix
+++ nix/pkgs/appview-static-files.nix
@@ -22,7 +22,7 @@ runCommandLocal "appview-static-files" {
   cp -rf ${lucide-src}/*.svg icons/
   cp -f ${inter-fonts-src}/web/InterVariable*.woff2 fonts/
   cp -f ${inter-fonts-src}/web/InterDisplay*.woff2 fonts/
-  cp -f ${ibm-plex-mono-src}/fonts/complete/woff2/IBMPlexMono-Regular.woff2 fonts/
+  cp -f ${ibm-plex-mono-src}/fonts/complete/woff2/IBMPlexMono*.woff2 fonts/
   # tailwindcss -c $src/tailwind.config.js -i $src/input.css -o tw.css won't work
   # for whatever reason (produces broken css), so we are doing this instead
   cd ${src} && ${tailwindcss}/bin/tailwindcss -i input.css -o $out/tw.css
