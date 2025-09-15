67644f448c1723f6dfaac01fae4ec40259ee6ba5
oppiliappan <me@oppi.li>
2025-09-15 10:13:06 +0100

    appview/db: support DID formats in labels
    Signed-off-by: oppiliappan <me@oppi.li>

--- appview/validator/validator.go
+++ appview/validator/validator.go
@@ -3,16 +3,19 @@ package validator
 import (
 	"tangled.sh/tangled.sh/core/appview/db"
 	"tangled.sh/tangled.sh/core/appview/pages/markup"
+	"tangled.sh/tangled.sh/core/idresolver"
 )
 
 type Validator struct {
 	db        *db.DB
 	sanitizer markup.Sanitizer
+	resolver  *idresolver.Resolver
 }
 
-func New(db *db.DB) *Validator {
+func New(db *db.DB, res *idresolver.Resolver) *Validator {
 	return &Validator{
 		db:        db,
 		sanitizer: markup.NewSanitizer(),
+		resolver:  res,
 	}
 }
